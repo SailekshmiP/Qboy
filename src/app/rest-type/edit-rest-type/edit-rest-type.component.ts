0f1048587480454772f0395dc8f521e3c4215c83
sailekshmip <sailekshmip@gmail.com>
2021-02-17 14:40:36 +0530

    new

---
+++ src/app/rest-type/edit-rest-type/edit-rest-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-rest-type',
+  templateUrl: './edit-rest-type.component.html',
+  styleUrls: ['./edit-rest-type.component.css']
+})
+export class EditRestTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
